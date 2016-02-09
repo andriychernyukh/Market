36ae5de5ae4ceb1839666f78aefa660fa8042ae7
Andriy Chernyukh <andriychernyukh@gmail.com>
2016-02-10 00:45:07 +0200

    Create Layout

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Market</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <div class="all-content-wrapper">
-            <div class="header">
-                Market
-            </div>
-            <div class="content">
-                <?php
-                include 'list.php';
-                ?>
-            </div>
-        </div>
-    </body>
-</html>
+<?php
+
+include 'Layout/_header.php';
+include 'list.php';
+include 'Layout/_footer.php';
+?>
+
+
 
 
